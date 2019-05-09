2dbe51e4306712c4ced351c506a48e86266b0147
Davis Truong <buckets_truong@Daviss-MacBook-Pro.local>
2019-05-08 20:29:24 -0600

    queries for comparison and exist or does not exist

--- vampire_app/app.js
+++ vampire_app/app.js
@@ -25,15 +25,127 @@ Vampire.collection.insertMany(vampireData,(err, data) => {
 // ### Add the vampire data that we gave you
 
 // ### Add some new vampire data
-
+// Vampire.create([
+//   {
+//    name:'kobe',
+//    hair_color:'black',
+//    eye_color:'brown',
+//    gender:'m'
+//   },
+//    if(error){
+//      console.log(error);
+//    } else {
+//      console.log(Vampire);
+//    }
+// ])
+// Vampire.create([
+//   {
+//     name:'Jes',
+//     hair_color:'blonde',
+//     eye_color:'green',
+//     gender: 'f'
+//   },
+//   if(error){
+//     console.log(error);
+//   } else {
+//     console.log(Vampire);
+//   }
+// ])
+// Vampire.create([
+//   {
+//     name:'Bridgette',
+//     hair_color:'brown',
+//     eye_color:'blue',
+//     gender: 'f'
+//   },
+//   if(error){
+//     console.log(error);
+//   } else {
+//     console.log(Vampire);
+//   }
+// ])
+// Vampire.create([
+//   {
+//     name:'reinhart',
+//     hair_color:'brown',
+//     eye_color:'blue',
+//     gender: 'm'
+//   },
+//   if(error){
+//     console.log(error);
+//   } else {
+//     console.log(Vampire);
+//   }
+// ])
 /////////////////////////////////////////////////
 // ## QUERYING
 /////////////////////////////////////////////////
 // ### Select by comparison
-
+// vampire.find({gender: 'f'}}, (error, vampire) => {
+//   if(error) {
+//     console.log(error);
+//   } else {
+//     console.log(vampire);
+//   }
+// })
+// vampire.find({victims:{$gt: 500}}, (error, vampire) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(vampire);
+//   }
+// })
+// vampire.find({victims:{$lt: 150}}, (error, vampire) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(vampire);
+//   }
+// })
+// vampire.find({victims:{$ne: 210234}}, (error, vampire) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(vampire);
+//   }
+// })
+// vampire.find({victims:{$gt: 150, $lt: 500}}, (error, vampire) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(vampire);
+//   }
+// })
 /////////////////////////////////////////////////
 // ### Select by exists or does not exist
-
+// vampire.find({title:{$exists: true}}, (error, vampire) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(vampire);
+//   }
+// })
+// vampire.find({victims:{$exists: false}}, (error, vampire) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(vampire);
+//   }
+// })
+// vampire.find({title:{$exists: true}, victims:{$exists:false}}, (error, vampire) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(vampire);
+//   }
+// })
+// vampire.find({victims:{$exists: true}, victims:{$gt:1000}}, (error, vampire) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(vampire);
+//   }
+// })
 /////////////////////////////////////////////////
 // ### Select with OR
 
@@ -50,11 +162,23 @@ Vampire.collection.insertMany(vampireData,(err, data) => {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // ## UPDATE
-
+app.put('/:id', (req, res) => {
+  vampire.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedVampire) => {
+    updatedVampire.save((err, savedUpdatedVampire)
+    })
+  })
+})
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // ## REMOVE
-
+app.delete('/:id', (req, res) => {
+  vampire.findByIdAndRemove(req.params.id, (err, deletedVampire) => {
+    if(err){
+      res.send(err)
+     } else {
+    }
+  })
+})
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 
